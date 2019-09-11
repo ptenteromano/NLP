babd49205fc0843443a4d04245c5762f3f2cfb63
Philip Tenteromano <tenteromano.p@gmail.com>
2019-09-11 17:44:04 -0400

    naive bayes on speech data

---
+++ udemy/spamdetection.py
@@ -0,0 +1,19 @@
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('spambase.data').values
+np.random.shuffle(data)
+
+X = data[:,:48]
+Y = data[:, -1]
+
+Xtrain = X[:-100,]
+Ytrain = Y[:-100,]
+Xtest = X[-100:,]
+Ytest = Y[-100:,]
+
+modelNB = MultinomialNB()
+modelNB.fit(Xtrain, Ytrain)
+print("NB score:", modelNB.score(Xtest, Ytest))
